513913384b7334a361961f5003f2a40f36ba0f2d
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-05-18 20:52:21 +0530

    add userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+//  /:username/submissions gives all submissions of that user
+// /:username/dashboard shows dashboard of that user
+const router=require('express').Router();
+
+router.get('')
+
+
+
+module.exports=router;
