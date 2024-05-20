2c8d1346ea3dc472a590f76d8f6e1e9b5a5c304e
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-05-20 16:16:34 +0530

    update problemRoutes.js

--- backend/routes/problemRoutes.js
+++ backend/routes/problemRoutes.js
@@ -1,17 +1,46 @@
 const router=require('express').Router();
 const ProblemModel=require('../models/ProblemModel');
+const jwt=require('jsonwebtoken');
 
 
+router.get('/getall',async function(req,res){
+  //send problems for home page
+});
+router.post('/create',async function(req,res){
 
-router.get('/getproblems',async function(req,res){
+  //check whether user is authenticated and have permission to do that or not
+  
+  //verify jwt
+  //extract token from cookies
+  console.log(req.cookies);
+  const {token}=req.cookies;
+  const payload=jwt.verify(token,process.env.SECRET_KEY);
+  console.log(payload);
+  //create problem and return 
+  //req.body contains dfetails
+  const{title,description,sampleinput,sampleoutput,rating}=req.body;
+  if(!(title && description &&sampleinput &&sampleoutput &&rating))
+    {
+      return res.status(400).send("provide all details for problem");
+    }
+  const problem={
+    title,
+    description,
+    sampleinput,
+    sampleoutput,
+    rating    
+  };
 
-  //check user is authenticated or not
-  const data=await ProblemModel.find();
-  res.status(200).json({
-    data
-  })
-
-  //send problems 
+  const newproblem=new ProblemModel(problem);
+  try{
+    await newproblem.save();
+  }
+  catch(err)
+  {
+    console.log(err);
+    res.status(400).send("something went wrong");
+  }
+  res.status(201).json(newproblem);
 })
 
 
