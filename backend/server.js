52af5ee65c44d4329c55515a5be2a854d21d7d1d
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-05-21 12:32:43 +0530

    minor update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 const cookieparser=require('cookie-parser');
+const cors=require('cors');
 const path=require('path');
 const { type } = require('os');
 const authRoute=require(path.join(__dirname,'routes','authRoutes.js'));
@@ -11,6 +12,11 @@ const problemRoute=require(path.join(__dirname,'routes','problemRoutes.js'));
 const app=express();
 dotenv.config();
 
+
+app.use(cors({
+  origin:'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieparser());
