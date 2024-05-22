036ccd00200277dae298804096daf09140cf7446
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-05-22 19:30:11 +0530

    update Problemdescription page

--- frontend/src/pages/ProblemDesc.jsx
+++ frontend/src/pages/ProblemDesc.jsx
@@ -9,7 +9,8 @@ function ProblemDesc()
   const {id}=useParams();
   const[loading,setLoading]=useState(true);
   const[auth,setAuth]=useState(false);
-  const[descdata,setdescdata]=useState(null);
+  const[code,setCode]=useState("");
+  const[descData,setdescData]=useState(null);
   const navigate=useNavigate();
   useEffect(()=>{
 
@@ -27,8 +28,9 @@ function ProblemDesc()
         }
         setAuth(true);
         const obj=await response.json();
-        const temp=<Problembox obj={obj}/>;
-        setdescdata(temp);
+        setdescData(obj);
+        // const temp=<Problembox obj={obj}/>;
+        // setdescdatajsx(temp);
     }
     getData();
   },[]);
@@ -41,17 +43,73 @@ function ProblemDesc()
       {
         return <Loading/>;
       }
+      //problembox is child component contains input ,output box to handle change
+      //in child component we will pass functions from parent to child and child calls
+      //this functions and sets state variable of parent here child
+      //will change sample input only output will be provided by browser
+
+
+      //state uplifting
+      //handlinputData when user pastes input
+      function handleInputChange(obj)
+      {
+        //here obj is modified obj passsed by chid component 
+        //i.e. we have obj with updated sampleinput
+        setdescData(obj);
+      }
+      //handleCodeChange
+      function handleCodeChange(e)
+      {
+        setCode(e.target.value);
+        console.log(e.target.value);
+      }
+      //handleRun
+      async function handleRun()
+      {
+        // window.alert("running 🏃‍♂️🏃‍♂️🏃💨 !!!")
+        //send post request server and get output 
+        const response=await fetch(`http://localhost:4000/user/run/${id}`,{
+          method:'POST',
+          credentials:'include',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify({code:code,
+            input:descData.sampleinput
+          })
+        })
+        if(!response.ok)
+          {
+            navigate('/login');
+            return;
+          }
+          const data=await response.json();
+          //data is object contains output key
+          setdescData({
+            ...descData,
+            sampleoutput:data.output
+          })
+          
+          console.log('ok from client side');
+      }
+      //handleSubmit
+      function handleSubmit()
+      {
+        window.alert("baby ,Calm down !!!😩😩")
+      }
   return(
     <div className={styles["problemdesc-container"]}>
       <div className={styles["problemdesc-container__left"]}>
         <p>compiler</p>
-        <textarea placeholder="//code here"></textarea>
+        <textarea placeholder="//code here" name="code" onChange={handleCodeChange}></textarea>
         <div className={styles["pdesc-footer"]}>
-          <button>Run</button>
-          <button>Submit</button>
+          <button onClick={handleRun}>Run</button>
+          <button onClick={handleSubmit}>Submit</button>
         </div>
       </div>
-      <div className={styles["problemdesc-container__right"]}>{descdata}</div>
+      <div className={styles["problemdesc-container__right"]}>
+       {descData?<Problembox obj={descData} handleInputChange={handleInputChange} />:<Loading />}
+      </div>
     </div>
   );
 }
