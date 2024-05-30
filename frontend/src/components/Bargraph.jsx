838d8588628033b650945ac9556033328d7218b2
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-05-30 16:26:41 +0530

    add Dashboard componenets

--- frontend/src/components/Bargraph.jsx
+++ frontend/src/components/Bargraph.jsx
@@ -1,68 +1,23 @@
-import { BarChart, Bar,XAxis, YAxis, CartesianGrid, Tooltip, Legend ,Rectangle} from 'recharts';
+import { BarChart, Bar,XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer ,Legend,Rectangle} from 'recharts';
 
 const data = [
-  {
-    rating: '800',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    rating: '900',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  { rating: 500, solved: 5 },
+  { rating: 1000, solved: 3 },
+  {rating:1400,solved:3},
+  { rating: 1500, solved: 4 },
+  { rating: 2000, solved: 6 },
 ];
 export default function Bargraph() {
   return (
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip shared={false} trigger="click" />
+    <ResponsiveContainer width="70%" height={500} >
+    <BarChart data={data} margin={{ top:20 ,right:0}} barGap={-2}>
+      <CartesianGrid strokeDasharray="5 5" />
+      <XAxis dataKey="rating" label={{ value: 'Problem Rating', position: 'insideBottomRight', offset: -1 }} />
+      <YAxis label={{ value: 'Problems Solved', angle: -90, position: 'insideLeft' }} />
+      <Tooltip />
       <Legend />
-      <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />}  />
+      <Bar dataKey="solved" fill="#FF3366"  barSize={40}/>
     </BarChart>
+  </ResponsiveContainer>
   );
 }
