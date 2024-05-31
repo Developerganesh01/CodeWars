b7bfeb477cd55a501a3f0404e0059c81a5dedc28
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-05-31 19:08:59 +0530

    update userSubmissionController.js

--- backend/controllers/userSubmissionController.js
+++ backend/controllers/userSubmissionController.js
@@ -1,11 +1,12 @@
 
 const path = require("path");
-const { writeFile } = require("node:fs/promises");
-const { exec} = require("promisify-child-process");
-const { v4: uuidv4 } = require("uuid");
 const ProblemModel = require(path.join(__dirname,"..","models","ProblemModel.js"));
 const SubmissionModel = require(path.join(__dirname,"..","models","SubmissionModel.js"));
 const TestCaseModel = require(path.join(__dirname,"..","models","TestcaseModel.js"));
+
+const compileCode=require(path.join(__dirname,"..","services","codeCompilation","compileCode.js"));
+const executeCode=require(path.join(__dirname,"..","services","codeExecution","executeCode.js"));
+
 
 async function getAllSubmissions (req, res) {
 
@@ -44,70 +45,89 @@ async function submitCode(req, res) {
   const problemId = req.params.id;
   const username=req.username;
 
-  //submission schema=>submissionTime,code,verdict,problemId,username
+  //submission schema=>submissionTime,code,verdict,problemId,username,language
   //from client side we have alredy collected problemId,username
   //now get code and save submissionTime
   const submissionTime = new Date();
-  const { code } = req.body;
+  const { code,language} = req.body;
   //run code and give verdict it is also a asynchronus work
   let verdict = "";
   //run testcases to get verdict
   //get array of object of testcases for this problemId
   const testcaseData = await TestCaseModel.find({ problemId });
-  //array of obj ,obj contains id,input,output,problemId
+  //array of obj ,obj contains _id,input,output,problemId
   //for each testcase run code and match output with computed output
-  const codefilepath = path.join(__dirname,"..","code",`${uuidv4()}${problemId}.cpp`);
-  const exefilepath = path.join(__dirname,"..","code",`${uuidv4()}${problemId}.exe`);
+  // console.log("testcaseData: "+testcaseData);
   //compile first
-  await writeFile(codefilepath, code);
-  const child = exec(`g++ ${codefilepath} -o ${exefilepath}`);
-  const { stdout, stderr } = await child;
-  if (stderr) {
-    console.log(stderr.split("\n"));
-    res.status(400).send("compilation error");
-    return;
+  let exeFilePath;
+  try{
+   exeFilePath=await compileCode(language,code);
+  //  console.log("exefile: "+exeFilePath);
+  }catch(err){
+    // console.log(err.message.split(".exe")[1].split(".cpp:")[1]);
+    const submission = new SubmissionModel({
+      submissionTime,
+      code,
+      verdict:"compilation error",
+      problemId,
+      username,
+      language
+    });
+   await submission.save();
+    return res.status(400).json({
+      verdict:"compilation Error",
+      msg:err.message
+    });
   }
-  let outputpromises = testcaseData.map(async (testcase, index) => {
-    const child = exec(`${exefilepath}`);
-    child.stdin.write(testcase.input);
-    child.stdin.end();
-    const { stdout, stderr } = await child;
-    if (stderr) {
+  //executeCode(language,code,output,input,compiledFilePath);
+  let outputpromises = testcaseData.map(async function(testcase)  {
+    try{
+      const result=await executeCode(language,code,testcase.output,testcase.input,exeFilePath);
+      console.log("testCase result: "+result);
+      return result;
+    }catch(err)
+    {
       return {
-        verdict: "failed",
-        stderr,
-      };
-    } else {
-      return { verdict: "running", stdout };
+        verdict:"error"
+      }
     }
   });
+  // console.log(outputpromises);
   try {
     outputpromises = await Promise.all(outputpromises);
   } catch (err) {
     return res.status(400).json(err);
   }
-  console.log(outputpromises);
-  let err = false;
-  for (let index = 0; index < outputpromises.length; index += 1) {
-    //stdout may contain line break and other things like /r/n to remove this use string.trim()
-    //console.log(outputpromises[index].verdict,outputpromises[index].stdout.trim(),testcaseData[index].output);
-    if (outputpromises[index].verdict === "failed" ||
-      outputpromises[index].stdout.trim() !== testcaseData[index].output){
-      err = true;
-      break;
+  // console.log(outputpromises);
+  let err=false;
+  let cerr=false;
+  for(let index=0;index<outputpromises.length;index+=1){
+    if(outputpromises[index].verdict==="error")
+      {
+        cerr=true;
+        break;
+      }
+    else if(outputpromises[index].verdict==="wrong answer"){
+      err=true;
     }
   }
-  if (!err) {
-    verdict = "accepted";
-  } else {
-    verdict = "wrong answer";
-  }
+  if(cerr)
+    {
+      verdict="compilation error";
+    }else if(err)
+      {
+        verdict="wrong answer";
+      }
+      else{
+        verdict="accepted";
+      }
   const submission = new SubmissionModel({
     submissionTime,
     code,
     verdict,
     problemId,
     username,
+    language
   });
   try {
     await submission.save();
@@ -115,41 +135,43 @@ async function submitCode(req, res) {
     console.log(err);
     return res.status(400).send("something wrong!!!");
   }
-  if (!err) {
-    res.status(201).json(submission);
-  } else {
-    res.status(400).send("wrong answer");
-  }
+  res.status(201).json(submission);
 }
 
-//handle run
-async function runCode (req, res) {
-  //without saving in submission collection send output by running code
-  // console.log(req.body);
-  const { code, input } = req.body;
-  const { id } = req.params;
-  //create file in code folder and write code in that file
-  const codefilepath = path.join(__dirname, "..", "code", `f${id}.cpp`);
-  const exefilepath = path.join(__dirname, "..", "code", `f${id}.exe`);
-  try {
-    await writeFile(codefilepath, code);
-    const child = exec(`g++ ${codefilepath} -o ${exefilepath} && ${exefilepath}`);
-    child.stdin.write(input);
-    child.stdin.end();
-    const { stdout, stderr } = await child;
-    if (stderr) {
-      console.log(stderr);
-      return res.status(200).json({
-        output: "compilation error",
-      });
-    }
-    res.status(200).json({
-      output: stdout,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(400).send("something wrong");
-  }
+// //handle run
+// async function runCode (req, res) {
+//   //without saving in submission collection send output by running code
+//   // console.log(req.body);
+//   const { code, input } = req.body;
+//   const { id } = req.params;
+//   //create file in code folder and write code in that file
+//   const codefilepath = path.join(__dirname, "..", "code", `f${id}.cpp`);
+//   const exefilepath = path.join(__dirname, "..", "code", `f${id}.exe`);
+//   try {
+//     await writeFile(codefilepath, code);
+//     const child = exec(`g++ ${codefilepath} -o ${exefilepath} && ${exefilepath}`);
+//     child.stdin.write(input);
+//     child.stdin.end();
+//     const { stdout, stderr } = await child;
+//     if (stderr) {
+//       console.log(stderr);
+//       return res.status(200).json({
+//         output: "compilation error",
+//       });
+//     }
+//     res.status(200).json({
+//       output: stdout,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(400).send("something wrong");
+//   }
+// }
+function runCode(req,res)
+{
+  res.status(200).json({
+    output:"runs"
+  })
 }
 
 module.exports ={getAllSubmissions,runCode,submitCode}
