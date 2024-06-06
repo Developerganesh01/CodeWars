fef05f9cc0bf946bae24b379825355b5e4d7baf4
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-06-06 12:54:24 +0530

    add viewSubmission route

--- backend/routes/userSubmissionRoutes.js
+++ backend/routes/userSubmissionRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const path=require("path");
-const {getAllSubmissions,runCode,submitCode}=require(path.join(__dirname,"..","controllers","userSubmissionController.js"));
+const {getAllSubmissions,runCode,submitCode,getSubmission}=require(path.join(__dirname,"..","controllers","userSubmissionController.js"));
 
 router.get("/getsubmissions",getAllSubmissions);
+router.get("/viewsubmission/:id",getSubmission);
 router.post("/submit/:id",submitCode); 
 router.post("/run/:id",runCode);
 
