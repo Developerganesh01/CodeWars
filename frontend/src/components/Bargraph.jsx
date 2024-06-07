d26e91b2b5490eb7a0f82f3d2cf43e9e15696f4d
Developerganesh01 <137500512+Developerganesh01@users.noreply.github.com>
2024-06-08 02:21:35 +0530

    update Bargraoh.jsx

--- frontend/src/components/Bargraph.jsx
+++ frontend/src/components/Bargraph.jsx
@@ -1,13 +1,13 @@
 import { BarChart, Bar,XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer ,Legend,Rectangle} from 'recharts';
 
-const data = [
-  { rating: 500, solved: 5 },
-  { rating: 1000, solved: 3 },
-  {rating:1400,solved:3},
-  { rating: 1500, solved: 4 },
-  { rating: 2000, solved: 6 },
-];
-export default function Bargraph() {
+// const data = [
+//   { rating: 500, solved: 5 },
+//   { rating: 1000, solved: 3 },
+//   {rating:1400,solved:3},
+//   { rating: 1500, solved: 4 },
+//   { rating: 2000, solved: 6 },
+// ];
+export default function Bargraph({data}) {
   return (
     <ResponsiveContainer width="70%" height={500} >
     <BarChart data={data} margin={{ top:20 ,right:0}} barGap={-2}>
